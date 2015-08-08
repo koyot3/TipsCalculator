8e437954601acd45306a714e6156a669c5abd0d8
admin <admin@admins-iMac.local>
2015-08-08 22:24:00 +0700

    Add images and mapper

--- TipsCalculator/TipsCalculator/ViewController.swift
+++ TipsCalculator/TipsCalculator/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtBillAmount: UITextField!
+    
+    @IBOutlet weak var lblTipsAmount: UILabel!
+    @IBOutlet weak var lblTipsRate: UILabel!
+    @IBOutlet weak var lblTotalAmount: UILabel!
+    
+    @IBOutlet weak var sldTipsRate: UISlider!
+
+    var tipsRate = 0.0
+    var billAmount = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Read the reference 
+        // Default rate
+        
+        // Read the saved bundle
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBillAmount(sender: AnyObject) {
+    }
 
+    @IBAction func updateTipsRate(sender: AnyObject) {
+    }
 }
 
