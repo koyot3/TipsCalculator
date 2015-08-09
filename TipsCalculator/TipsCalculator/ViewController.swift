f34aa932e127a4ef8f774bd93d7fa391fb222ef5
admin <admin@admins-iMac.local>
2015-08-09 17:12:29 +0700

    Default data for icon & Main Storyboard

--- TipsCalculator/TipsCalculator/ViewController.swift
+++ TipsCalculator/TipsCalculator/ViewController.swift
@@ -22,11 +22,25 @@ class ViewController: UIViewController {
     var tipsRate = 0.0
     var billAmount = 0.0
     
+    var tipsType:NSString!
+    
+    var rateDefault:NSUserDefaults!
+    
+    var myDictOfDict:NSDictionary = [
+        "restaurant" : ["name": "Restaurant", "rate": 10, "icon": "icon-restaurant.png"],
+        "taxi" : ["name": "Taxi", "rate": 15, "icon": "icon-taxi.png"],
+        "salon" : ["name": "Beauty Salon", "rate": 15, "icon": "icon-salon.png"],
+        "hotel" : ["name": "Hotel", "rate": 10, "icon": "icon-hotel.png"],
+    ];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Read the reference 
-        // Default rate
-        
+        rateDefault = NSUserDefaults()
+        tipsRate = rateDefault.doubleForKey("tipsRate")
+        billAmount = rateDefault.doubleForKey("billAmount")
+        updateUI()
+        updateAmount()
         // Read the saved bundle
         
     }
@@ -37,11 +51,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func updateBillAmount(sender: AnyObject) {
+        if(txtBillAmount.text != "")
+        {
+            billAmount = (txtBillAmount.text as NSString).doubleValue
+        } else{
+            billAmount = 0.0
+        }
+        updateAmount()
     }
 
     @IBAction func updateTipsRate(sender: AnyObject) {
+        tipsRate = Double(sldTipsRate.value)
+        lblTipsRate.text = String(format: "%.f%%", tipsRate)
+        updateAmount()
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        rateDefault.setDouble(tipsRate, forKey: "tipsRate")
+        rateDefault.setDouble(billAmount, forKey: "billAmount")
+        rateDefault.synchronize()
+    }
+    
+    func updateAmount(){
+        lblTipsAmount.text = String(format:"%.f", billAmount * tipsRate / 100)
+        lblTotalAmount.text = String(format:"%.f", billAmount * tipsRate / 100 + billAmount)
+    }
+    
+    func updateUI(){
+        txtBillAmount.text = String(format: "%.f", billAmount)
+        sldTipsRate.value = Float(tipsRate)
+        lblTipsRate.text = String(format: "%.f%%", tipsRate)
+    }
     
 }
 
